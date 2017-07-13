3ec03c81c268374d079374ce3eeedce7433c58a0
xiaoyaoyimei <1206902591@qq.com>
2017-07-13 10:59:27 +0800

    router

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="login">
+    <h1>I am a title.</h1>
+    <a> written by {{ msg }} </a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+/* eslint-disable */
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
